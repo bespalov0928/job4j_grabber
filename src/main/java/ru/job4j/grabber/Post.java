2fc08e0791827660ca90d23d89dffee636fabc03
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-11-14 11:37:05 +0300

    2/2.3/6/2.3. Загрузка деталей поста. [#314178]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,9 +1,64 @@
 package ru.job4j.grabber;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Post {
 
     String avtor = "";
     String description = "";
     String dateCreation = "";
 
+    public Post(String url) throws IOException {
+        Map filds = postParser(url);
+        avtor = (String) filds.get("avtor");
+        description = (String) filds.get("description");
+        dateCreation = (String) filds.get("dateCreation");
+    }
+
+    public static Map postParser(String url) throws IOException {
+
+        HashMap map = new HashMap();
+        map.put("avtor", "");
+        map.put("description", "");
+        map.put("dateCreation", "");
+
+        String avtor = "";
+        String description = "";
+        String dateCreation = "";
+
+        //Post post = new Post();
+
+        Document doc = Jsoup.connect(url).get();
+        Elements rowMsgTableArr = doc.select(".msgTable");
+
+        Element rowMsgTable = rowMsgTableArr.get(0);
+        Elements rowMsgBodyArr = rowMsgTable.select(".msgBody");
+
+        Element rowAvtor = rowMsgBodyArr.get(0);
+        Element href = rowAvtor.child(0);
+        avtor = href.text();
+
+        Element rowDescription = rowMsgBodyArr.get(1);
+        description = rowDescription.text();
+
+        Elements rowMsgFooterArr = rowMsgTable.select(".msgFooter");
+        dateCreation = rowMsgFooterArr.get(0).text();
+        String[] dateCreationArr = dateCreation.split(",");
+
+//        post.avtor = avtor;
+//        post.description = description;
+//        post.dateCreation = dateCreation;
+
+        map.put("avtor", avtor);
+        map.put("description", description);
+        map.put("dateCreation", dateCreationArr[0]);
+        return map;
+    }
 }
--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import javax.xml.crypto.Data;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -16,19 +17,6 @@ public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
 
-        HashMap mapMonths = new HashMap();
-        mapMonths.put("янв", "01");
-        mapMonths.put("фев", "02");
-        mapMonths.put("мар", "03");
-        mapMonths.put("апр", "04");
-        mapMonths.put("май", "05");
-        mapMonths.put("июн", "06");
-        mapMonths.put("июл", "07");
-        mapMonths.put("авг", "08");
-        mapMonths.put("сен", "09");
-        mapMonths.put("окт", "10");
-        mapMonths.put("ноя", "11");
-        mapMonths.put("дек", "12");
 
         String url = "";
 
@@ -47,40 +35,70 @@ public class SqlRuParse {
                 if (index % 2 != 0) {
                     //System.out.println(rowAltCol.get(index * 2 - 1).attr("altCol"));
                     String dataString = rowAltCol.get(index * 2 - 1).text();
+                    Date dateNew = formatDate(dataString);
+
                     System.out.println(dataString);
-
-                    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy", Locale.ENGLISH);
-                    String[] dataTimeArr = dataString.split(",");
-                    String[] dataArr = dataTimeArr[0].split(" ");
-
-                    Calendar calendar = new GregorianCalendar();
-                    String day = "";
-                    String manch = "";
-                    String year = "";
-
-                    if (dataArr.length == 1) {
-                        if (dataArr[0].equals("вчера")) {
-                            Date getTime = calendar.getTime();
-                            day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH) - 1);
-                            manch = String.valueOf(calendar.get(Calendar.MONTH));
-                            year = String.valueOf(calendar.get(Calendar.YEAR));
-
-                        } else if (dataArr[0].equals("сегодня")) {
-                            day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
-                            manch = String.valueOf(calendar.get(Calendar.MONTH));
-                            year = String.valueOf(calendar.get(Calendar.YEAR));
-                        }
-                    } else {
-                        day = dataArr[0];
-                        manch = (String) mapMonths.get(dataArr[1]);
-                        year = dataArr[2].replace(",", "");
-                    }
-
-                    String dataInStringNew = String.format("%s-%s-%s", day, manch, year);
-                    Date dateNew = formatter.parse(dataInStringNew);
                     System.out.println(dateNew);
                 }
             }
         }
+
+        Post post = new Post("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        //post.load("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        post.dateCreation = String.valueOf(formatDate(post.dateCreation));
+        System.out.println(post.dateCreation);
     }
+
+    public static Date formatDate(String dataString) throws ParseException {
+
+        HashMap mapMonths = new HashMap();
+        mapMonths.put("янв", "01");
+        mapMonths.put("фев", "02");
+        mapMonths.put("мар", "03");
+        mapMonths.put("апр", "04");
+        mapMonths.put("май", "05");
+        mapMonths.put("июн", "06");
+        mapMonths.put("июл", "07");
+        mapMonths.put("авг", "08");
+        mapMonths.put("сен", "09");
+        mapMonths.put("окт", "10");
+        mapMonths.put("ноя", "11");
+        mapMonths.put("дек", "12");
+
+
+        Date date = null;
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy", Locale.ENGLISH);
+        String[] dataTimeArr = dataString.split(",");
+        String[] dataArr = dataTimeArr[0].split(" ");
+
+        Calendar calendar = new GregorianCalendar();
+        String day = "";
+        String manch = "";
+        String year = "";
+
+        if (dataArr.length == 1) {
+            if (dataArr[0].equals("вчера")) {
+                Date getTime = calendar.getTime();
+                day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH) - 1);
+                manch = String.valueOf(calendar.get(Calendar.MONTH));
+                year = String.valueOf(calendar.get(Calendar.YEAR));
+
+            } else if (dataArr[0].equals("сегодня")) {
+                day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+                manch = String.valueOf(calendar.get(Calendar.MONTH));
+                year = String.valueOf(calendar.get(Calendar.YEAR));
+            }
+        } else {
+            day = dataArr[0];
+            manch = (String) mapMonths.get(dataArr[1]);
+            year = dataArr[2].replace(",", "");
+        }
+
+        String dataInStringNew = String.format("%s-%s-%s", day, manch, year);
+        Date dateNew = formatter.parse(dataInStringNew);
+
+        return dateNew;
+    }
+
+
 }
