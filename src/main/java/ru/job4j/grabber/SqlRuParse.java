0cdee3c6b291d41e33b694e75199e73336abfdad
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-11-14 09:00:22 +0300

    2/2.3/6/2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#314176]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -30,60 +30,56 @@ public class SqlRuParse {
         mapMonths.put("ноя", "11");
         mapMonths.put("дек", "12");
 
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        String url = "";
 
-        Elements row = doc.select(".postslisttopic");
-//        for (Element td : row) {
-//            Element href = td.child(0);
-//            System.out.println(href.attr("href"));
-//            System.out.println(href.attr(href.text()));
-//         }
-        Elements rowAltCol = doc.select(".altCol");
+        for (int list = 1; list <= 5; list++) {
+            url = String.format("https://www.sql.ru/forum/job-offers/%s", list);
+            //Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements rowAltCol = doc.select(".altCol");
 
-        for (int index = 0; index < row.size(); index++) {
-            Element href = row.get(index).child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.attr(href.text()));
+            for (int index = 0; index < row.size(); index++) {
+                Element href = row.get(index).child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.attr(href.text()));
 
-            if (index % 2 != 0) {
-                //System.out.println(rowAltCol.get(index * 2 - 1).attr("altCol"));
-                String dataString = rowAltCol.get(index * 2 - 1).text();
-                System.out.println(dataString);
+                if (index % 2 != 0) {
+                    //System.out.println(rowAltCol.get(index * 2 - 1).attr("altCol"));
+                    String dataString = rowAltCol.get(index * 2 - 1).text();
+                    System.out.println(dataString);
 
-                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy", Locale.ENGLISH);
-                String[] dataTimeArr = dataString.split(",");
-                String[] dataArr = dataTimeArr[0].split(" ");
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy", Locale.ENGLISH);
+                    String[] dataTimeArr = dataString.split(",");
+                    String[] dataArr = dataTimeArr[0].split(" ");
 
-                //Date date = new Date();
-                Calendar calendar = new GregorianCalendar();
+                    Calendar calendar = new GregorianCalendar();
+                    String day = "";
+                    String manch = "";
+                    String year = "";
 
-                //Data dataStringNew = date.getDate();
-                String day = "";
-                String manch = "";
-                String year = "";
+                    if (dataArr.length == 1) {
+                        if (dataArr[0].equals("вчера")) {
+                            Date getTime = calendar.getTime();
+                            day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH) - 1);
+                            manch = String.valueOf(calendar.get(Calendar.MONTH));
+                            year = String.valueOf(calendar.get(Calendar.YEAR));
 
-                if (dataArr.length == 1) {
-                    if (dataArr[0].equals("вчера")) {
-                        Date getTime = calendar.getTime();
-                        day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH) - 1);
-                        manch = String.valueOf(calendar.get(Calendar.MONTH));
-                        year = String.valueOf(calendar.get(Calendar.YEAR));
-
-                    } else if (dataArr[0].equals("сегодня")) {
-                        day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
-                        manch = String.valueOf(calendar.get(Calendar.MONTH));
-                        year = String.valueOf(calendar.get(Calendar.YEAR));
+                        } else if (dataArr[0].equals("сегодня")) {
+                            day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+                            manch = String.valueOf(calendar.get(Calendar.MONTH));
+                            year = String.valueOf(calendar.get(Calendar.YEAR));
+                        }
+                    } else {
+                        day = dataArr[0];
+                        manch = (String) mapMonths.get(dataArr[1]);
+                        year = dataArr[2].replace(",", "");
                     }
-                } else {
-                    day = dataArr[0];
-                    manch = (String) mapMonths.get(dataArr[1]);
-                    year = dataArr[2].replace(",", "");
-                }
 
-                String dataInStringNew = String.format("%s-%s-%s", day, manch, year);
-                //String dataInString = "07-jun-2013";
-                Date dateNew = formatter.parse(dataInStringNew);
-                System.out.println(dateNew);
+                    String dataInStringNew = String.format("%s-%s-%s", day, manch, year);
+                    Date dateNew = formatter.parse(dataInStringNew);
+                    System.out.println(dateNew);
+                }
             }
         }
     }
